209d062e6e678ce5d8002900cab4a2564ee65d72
Devkiran220 <168909902+devk220@users.noreply.github.com>
2024-05-05 18:23:51 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const cors=require('cors')
 const bodyParser = require('body-parser')
-const connectToMongo=require('./db')
+const connectToMongo=require('./zb')
 const schema=require('./userSchema')
 const mongo = require('mongoose')
 const multer=require('multer')
